9603dbe7ad39f1ebb75a5cc28e8dd0fb620eaae7
gnomeontherun <gnomation@gnomeontherun.com>
2010-08-04 04:25:24 +0000

    vintage import

---
+++ trunk/js/template.css.php
@@ -0,0 +1,7 @@
+<?php
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted index access' );
+?>
+<?php
+echo "<a href=\"http://www.joomlashack.com\" title=\"Joomla 1.5 Templates\">Joomla Templates by Joomlashack</a>";
+?>

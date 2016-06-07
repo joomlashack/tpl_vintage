54f535a97e3757631c0469688a740d5765a7b927
Valentín <htmgarcia@gmail.com>
2016-06-07 13:12:25 -0500

    Remove header image setting

--- code/functions.php
+++ code/functions.php
@@ -10,13 +10,6 @@
 // Restrict Access to within Joomla
 defined('_JEXEC') or die('Restricted access');
 
-$bg_image                       = $this -> params -> get('bg_image');
-if( $bg_image ) {
-    JFactory::getDocument()->addStyleDeclaration(
-        '#header {background-image: url(' . JURI::base() . $bg_image . ');}'
-    );
-}
-
 $enable_featured_module_button  = ($this -> params -> get('enable_featured_module_button', '0') != '0' ? false : true);
 $featured_text_button           = $this -> params -> get('featured_text_button', 'Featured');
 $template_style                 = $this -> params -> get('style', 'red');

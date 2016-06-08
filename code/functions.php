29ff34c4c50f7bb3efbb12f4dfbaa9b26b2b28c9
Valentín <htmgarcia@gmail.com>
2016-06-08 17:28:04 -0500

    Remove the old/new templating system
    Issue #6

--- code/functions.php
+++ code/functions.php
@@ -10,26 +10,5 @@
 // Restrict Access to within Joomla
 defined('_JEXEC') or die('Restricted access');
 
-$user							= JFactory::getUser();
 $enable_featured_module_button  = ($this -> params -> get('enable_featured_module_button', '0') != '0' ? false : true);
 $featured_text_button           = $this -> params -> get('featured_text_button', 'Featured');
-$template_style					= 'old';
-$theme_style					= ($this->params->get('style', 'red-new'));
-
-if (strpos($theme_style, '-new'))
-{
-	$template_style = 'new';
-}
-
-if (!is_null(JRequest::getVar('templateTheme', NULL)))
-{
-	$theme_style = JRequest::getVar('templateTheme');
-
-	if (strpos($theme_style, '-new'))
-	{
-		$template_style = 'new';
-	}
-	else {
-		$template_style = 'old';
-	}
-}

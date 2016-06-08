0265fdbe1f43c269ebf1c52fb562aee7bf76fd59
Valentín <htmgarcia@gmail.com>
2016-06-08 11:54:31 -0500

    Default style

--- code/functions.php
+++ code/functions.php
@@ -12,4 +12,4 @@ defined('_JEXEC') or die('Restricted access');
 
 $enable_featured_module_button  = ($this -> params -> get('enable_featured_module_button', '0') != '0' ? false : true);
 $featured_text_button           = $this -> params -> get('featured_text_button', 'Featured');
-$template_style                 = $this -> params -> get('style', 'red');
+$template_style                 = $this -> params -> get('style', 'red-new');
